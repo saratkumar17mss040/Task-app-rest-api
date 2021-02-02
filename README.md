3f64847bb900c018764b48db0ed13d4b363fb556
sarath <sarathdon9245@gmail.com>
2021-02-02 22:17:11 +0530

    🚀 Docs - update docs

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# task-app-rest-api
-A simple task application rest api where the users can create, update, delete tasks.
+# Task-app-rest-api
+
+A simple task-rest 😎 API where the users can create, update, delete task with basic JWT to authenticate and authorize requests.
+
+## Tech stacks used in building this API
+
+Hapi - Secured web framework.
+
+Joi - Schema description and data validation.
+
+Hapi-auth-jwt2 - Hapi.js authentication plugin / scheme using JWT.
+
+Json-web-token - JSON token implementation.
+
+Uuid - For the creation of RFC4122 UUID's.
+
+Dynamodb-local - NoSQL database that supports key-value and document data structures.
+
+Aws-sdk - AWS sdk for javascript in order to connect to dynamodb-local.
+
+Hapi-swagger - A swagger documentation UI generator plugin for hapijs.
+
+Vision - Template rendering plugin support for hapij.
+
+Inert - Static file and directory handlers plugin for hapijs.
+
+## Some of the other tools used in this project to follow best practices, 
+## maintain code quality and to have good development experience
+
+Eslint - A pluggable and configurable linter tool for identifying and reporting on patterns in JavaScript. Maintain your code quality with ease.
+
+Prettier - Prettier is an opinionated code formatter. It enforces a consistent style.
+
+Dotenv - Loads environment variables from .env file.
+
+Nodemon - Nodemon is a utility that will monitor for any changes in your source and automatically restart your server.
+
+## Installation:
+
+Before trying to run the project make sure you have installed node, dynamodb-local.
+
+## To run the project:
+
+1.Clone this repo  
+`git clone https://github.com/saratkumar17mss040/Task-app-rest-api.git`
+
+2.Npm install  
+`npm install`
+
+3.Npm start  
+`npm start`
+
+## Interaction and API testing
+
+Once the server started running open the browser with the localhost path as
+http://localhost:3000/documentation
+
+You will see swagger in which it provides the UI to test and call the API endpoints.
+
+Alternatively you can also use postman to test API calls.
+
+## Features
+
+-   Users will be able to signup / login.
+-   Users can create todo.
+-   Users can get todo.
+-   Users can update todo.
+-   Users can delete todo.
+
--- src/server/validations/validation.js
+++ src/server/validations/validation.js
@@ -3,22 +3,29 @@
 const Joi = require('joi');
 
 const signupSchema = Joi.object({
-    emailId: Joi.string().email().required().description('emailId for signup'),
+    emailId: Joi.string()
+        .email()
+        .required()
+        .description('emailId for signup')
+        .default('ramabot123@gmail.com'),
     password: Joi.string()
         .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
         .required()
-        .description('password for signup'),
+        .description('password for signup')
+        .default('456'),
 });
 
 const loginSchema = Joi.object({
     emailId: Joi.string()
         .email()
         .required()
-        .description('emailId of the registered user'),
+        .description('emailId of the registered user')
+        .default('sam123@gmail.com'),
     password: Joi.string()
         .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
         .required()
-        .description('password of the registered user'),
+        .description('password of the registered user')
+        .default('123'),
 });
 
 const getTasksSchema = Joi.object({
@@ -26,7 +33,8 @@ const getTasksSchema = Joi.object({
         .required()
         .min(36)
         .max(36)
-        .description('userId of type uuid - v4'),
+        .description('userId of type uuid - v4')
+        .default('cf6b6ee0-5f81-4166-b51b-d1994aaf3307'),
 });
 
 const createTaskSchema = Joi.object({
@@ -34,11 +42,16 @@ const createTaskSchema = Joi.object({
         .required()
         .min(36)
         .max(36)
-        .description('userId of type uuid - v4'),
-    todo: Joi.string().required().description('todo to create task'),
+        .description('userId of type uuid - v4')
+        .default('cf6b6ee0-5f81-4166-b51b-d1994aaf3307'),
+    todo: Joi.string()
+        .required()
+        .description('todo to create task')
+        .default('Buy laptop'),
     todoStatus: Joi.string()
         .required()
-        .description('todoStatus whether it is completed or not'),
+        .description('todoStatus whether it is completed or not')
+        .default('not completed'),
 });
 
 const updateTaskSchema = Joi.object({
@@ -46,20 +59,24 @@ const updateTaskSchema = Joi.object({
         .required()
         .min(36)
         .max(36)
-        .description('userId of type uuid - v4'),
+        .description('userId of type uuid - v4')
+        .default('cf6b6ee0-5f81-4166-b51b-d1994aaf3307'),
     updateTodoId: Joi.string()
         .required()
         .min(36)
         .max(36)
-        .description('updateTodoId for updating the specific todo'),
+        .description('updateTodoId for updating the specific todo')
+        .default('71a7a490-c132-42c8-ac45-91884a5e9508'),
     updateTodo: Joi.string()
         .required()
-        .description('updateTodo for actual todo update'),
+        .description('updateTodo for actual todo update')
+        .default('Buy popcorn'),
     updateTodoStatus: Joi.string()
         .required()
         .description(
             'updateTodoStatus to update whether the todo is completed or not',
-        ),
+        )
+        .default('completed'),
 });
 
 const deleteTaskSchema = Joi.object({
@@ -67,12 +84,14 @@ const deleteTaskSchema = Joi.object({
         .required()
         .min(36)
         .max(36)
-        .description('userId of type uuid - v4'),
+        .description('userId of type uuid - v4')
+        .default('cf6b6ee0-5f81-4166-b51b-d1994aaf3307'),
     deleteTodoId: Joi.string()
         .required()
         .min(36)
         .max(36)
-        .description('deleteTodoId to delete todo by id'),
+        .description('deleteTodoId to delete todo by id')
+        .default('71a7a490-c132-42c8-ac45-91884a5e9508'),
 });
 
 module.exports = {
