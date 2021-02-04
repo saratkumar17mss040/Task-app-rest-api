85eed13f83faad7c332fb2b789c77e695783ad7e
sarath <sarathdon9245@gmail.com>
2021-02-04 13:28:24 +0530

    🚀 Remove default values

--- src/server/schema/schema.js
+++ src/server/schema/schema.js
@@ -4,29 +4,22 @@ const Joi = require('joi');
 
 // All server requests schema data validation
 const signupSchema = Joi.object({
-    emailId: Joi.string()
-        .email()
-        .required()
-        .description('emailId for signup')
-        .default('ramabot123@gmail.com'),
+    emailId: Joi.string().email().required().description('emailId for signup'),
     password: Joi.string()
         .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
         .required()
-        .description('password for signup')
-        .default('456'),
+        .description('password for signup'),
 }).label('signupModel');
 
 const loginSchema = Joi.object({
     emailId: Joi.string()
         .email()
         .required()
-        .description('emailId of the registered user')
-        .default('sam123@gmail.com'),
+        .description('emailId of the registered user'),
     password: Joi.string()
         .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
         .required()
-        .description('password of the registered user')
-        .default('123'),
+        .description('password of the registered user'),
 }).label('loginModel');
 
 const getTasksSchema = Joi.object({
@@ -34,8 +27,7 @@ const getTasksSchema = Joi.object({
         .required()
         .min(36)
         .max(36)
-        .description('userId of type uuid - v4')
-        .default('cf6b6ee0-5f81-4166-b51b-d1994aaf3307'),
+        .description('userId of type uuid - v4'),
 }).label('getTasksModel');
 
 const createTaskSchema = Joi.object({
@@ -43,16 +35,11 @@ const createTaskSchema = Joi.object({
         .required()
         .min(36)
         .max(36)
-        .description('userId of type uuid - v4')
-        .default('cf6b6ee0-5f81-4166-b51b-d1994aaf3307'),
-    todo: Joi.string()
-        .required()
-        .description('todo to create task')
-        .default('Buy laptop'),
+        .description('userId of type uuid - v4'),
+    todo: Joi.string().required().description('todo to create task'),
     todoStatus: Joi.string()
         .required()
-        .description('todoStatus whether it is completed or not')
-        .default('not completed'),
+        .description('todoStatus whether it is completed or not'),
 }).label('createTaskModel');
 
 const updateTaskSchema = Joi.object({
@@ -60,24 +47,20 @@ const updateTaskSchema = Joi.object({
         .required()
         .min(36)
         .max(36)
-        .description('userId of type uuid - v4')
-        .default('cf6b6ee0-5f81-4166-b51b-d1994aaf3307'),
+        .description('userId of type uuid - v4'),
     updateTodoId: Joi.string()
         .required()
         .min(36)
         .max(36)
-        .description('updateTodoId for updating the specific todo')
-        .default('71a7a490-c132-42c8-ac45-91884a5e9508'),
+        .description('updateTodoId for updating the specific todo'),
     updateTodo: Joi.string()
         .required()
-        .description('updateTodo for actual todo update')
-        .default('Buy popcorn'),
+        .description('updateTodo for actual todo update'),
     updateTodoStatus: Joi.string()
         .required()
         .description(
             'updateTodoStatus to update whether the todo is completed or not',
-        )
-        .default('completed'),
+        ),
 }).label('updateTaskModel');
 
 const deleteTaskSchema = Joi.object({
@@ -85,14 +68,12 @@ const deleteTaskSchema = Joi.object({
         .required()
         .min(36)
         .max(36)
-        .description('userId of type uuid - v4')
-        .default('cf6b6ee0-5f81-4166-b51b-d1994aaf3307'),
+        .description('userId of type uuid - v4'),
     deleteTodoId: Joi.string()
         .required()
         .min(36)
         .max(36)
-        .description('deleteTodoId to delete todo by id')
-        .default('71a7a490-c132-42c8-ac45-91884a5e9508'),
+        .description('deleteTodoId to delete todo by id'),
 }).label('deleteTaskModel');
 
 // All server response schema model
@@ -119,7 +100,7 @@ const updateTaskResponseSchema = Joi.object({
 
 const deleteTaskResponseSchema = Joi.object({
     message: Joi.string(),
-}).label('deleteTaskResponseModel');
+}).label('deleteTaskResponseMode');
 
 const signupResponseSchema = Joi.object({
     message: Joi.string(),
